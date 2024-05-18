d27e11a07e77001dc1dc161f03f1ba73e737cd5c
Sanskar <64313600+sanskari27@users.noreply.github.com>
2024-05-18 17:52:28 +0530

    forgot password added

--- server/src/modules/auth/auth.route.ts
+++ server/src/modules/auth/auth.route.ts
@@ -9,7 +9,7 @@ router.route('/validate').all(VerifyUser).get(AuthController.validateLogin);
 router.route('/validate-client-id').all(VerifyUser).get(AuthController.validateClientID);
 
 router.route('/login').all(LoginValidator).post(AuthController.login);
-router.route('/forgot-password').patch(AuthController.forgotPassword);
+router.route('/forgot-password').post(AuthController.forgotPassword);
 
 router.route('/initiate-client').all(VerifyUser).post(AuthController.initiateWhatsapp);
 router.route('/reset-password/:id').all(VerifyUser).get(AuthController.resetPassword);
